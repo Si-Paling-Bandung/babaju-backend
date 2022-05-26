9dcefaf4463ecf8eb6934f7f66bdf84e51fe8a2d
M. Faiz Triputra <m.faiztpaduhai@gmail.com>
2022-05-26 18:55:15 +0700

    up

---
+++ database/migrations/2022_05_26_115040_create_education_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEducationCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('education_categories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('education_categories');
+    }
+}
